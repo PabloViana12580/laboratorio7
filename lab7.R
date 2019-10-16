bc5436cc71312ae4320bf58e4ccd75a67610c603
Sergio A. Marchena G <mar16387@uvg.edu.gt>
2019-10-16 12:56:11 -0600

    limpieza
    limpieza

--- lab7.R
+++ lab7.R
@@ -45,4 +45,31 @@ View(realData)
 
 # PREPROCESAMIENTO #
 
+#----------------- LIMPIEZA -----------------#
+# To lower
+
+realData$text <- tolower(realData$text)
+
+# reemplazar caracteres
+data$reviews.text <- gsub("@", "", data$reviews.text)
+data$reviews.text <- gsub("#", "", data$reviews.text)
+data$reviews.text <- gsub("'", "", data$reviews.text)
+
+data$reviews.title <- gsub("@", "", data$reviews.title)
+data$reviews.title <- gsub("#", "", data$reviews.title)
+data$reviews.title <- gsub("'", "", data$reviews.title)
+
+# Quitar stopwords, signos de puntuacion
+stopWords <- stopwords(kind = "en")
+
+data$reviews.text <- removeWords(data$reviews.text, stopWords)
+data$reviews.text <- removePunctuation(data$reviews.text)
+data$reviews.text <- removeNumbers(data$reviews.text)
+
+data$reviews.title <- removeWords(data$reviews.title, stopWords)
+data$reviews.title <- removePunctuation(data$reviews.title)
+data$reviews.title <- removeNumbers(data$reviews.title)
+
+# Quitar urls
+data$reviews.text<- gsub('http\\S+\\s*', '', data$reviews.text)
 
